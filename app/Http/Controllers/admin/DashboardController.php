acc4acb6654e716f66c6b18e6ae2a549e48d7272
RanaSikander44 <sikander@wistech.biz>
2024-11-25 16:19:28 +0500

    students

--- app/Http/Controllers/admin/DashboardController.php
+++ app/Http/Controllers/admin/DashboardController.php
@@ -3,12 +3,14 @@
 namespace App\Http\Controllers\admin;
 
 use App\Http\Controllers\Controller;
+use App\Models\student;
 use Illuminate\Http\Request;
 
 class DashboardController extends Controller
 {
     public function dashboard()
     {
-        return view('admin.dashboard');
+        $total_students = student::count();
+        return view('admin.dashboard', compact('total_students'));
     }
 }
--- resources/views/admin/Dashboard.blade.php
+++ resources/views/admin/Dashboard.blade.php
@@ -6,34 +6,40 @@
         <div class="col-12">
             <div class="card bg-white text-dark mb-4 rounded-2 mt-4">
                 <div class="card-body">
-                    <h5 class="ms-1">Welcome - User | Admin Dashboard</h5>
+                    <h5 class="ms-1">Welcome - {{ Auth::user()->name }} | Admin Dashboard</h5>
                     <div class="row mt-4">
                         <div class="col-md-3">
                             <div class="card bg-info text-white rounded-2 bg-gradient" style="border: none;">
                                 <h5 class="ms-3 mt-3">Student</h5>
                                 <p class="ms-3 mt-2">Total Students</p>
-                                <a class="h5 text-white fw-bold stretched-link ms-3 mb-3 text-decoration-none" href="#">2</a>
+                                <a class="h5 text-white fw-bold stretched-link ms-3 mb-3 text-decoration-none"
+                                    href="{{ route('applications') }}">{{ $total_students }}</a>
                             </div>
                         </div>
                         <div class="col-md-3">
-                            <div class="card text-white rounded-2 bg-gradient"  style="border: none; background-color: #8D5FF6;">
+                            <div class="card text-white rounded-2 bg-gradient"
+                                style="border: none; background-color: #8D5FF6;">
                                 <h5 class="ms-3 mt-3">Teachers</h5>
                                 <p class="ms-3 mt-2">Total Teachers</p>
-                                <a class="h5 text-white fw-bold stretched-link ms-3 mb-3 text-decoration-none" href="#">2</a>
+                                <a class="h5 text-white fw-bold stretched-link ms-3 mb-3 text-decoration-none"
+                                    href="#">2</a>
                             </div>
                         </div>
                         <div class="col-md-3">
                             <div class="card bg-primary text-white rounded-2 bg-gradient" style="border: none;">
                                 <h5 class="ms-3 mt-3">Parents</h5>
                                 <p class="ms-3 mt-2">Total Parents</p>
-                                <a class="h5 text-white fw-bold stretched-link ms-3 mb-3 text-decoration-none" href="#">2</a>
+                                <a class="h5 text-white fw-bold stretched-link ms-3 mb-3 text-decoration-none"
+                                    href="#">2</a>
                             </div>
                         </div>
                         <div class="col-md-3">
-                            <div class="card text-white rounded-2 bg-gradient"   style="border: none; background-color: #DC4BF1;">
+                            <div class="card text-white rounded-2 bg-gradient"
+                                style="border: none; background-color: #DC4BF1;">
                                 <h5 class="ms-3 mt-3">Staffs</h5>
                                 <p class="ms-3 mt-2">Total Staffs</p>
-                                <a class="h5 text-white fw-bold stretched-link ms-3 mb-3 text-decoration-none" href="#">2</a>
+                                <a class="h5 text-white fw-bold stretched-link ms-3 mb-3 text-decoration-none"
+                                    href="#">2</a>
                             </div>
                         </div>
                     </div>
@@ -41,7 +47,7 @@
             </div>
         </div>
     </div>
-    
+
 
 
     <div class="row">
