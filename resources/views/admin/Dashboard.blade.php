c7a38fb8331729a36bcfef6ab1f01d030a11813c
RanaSikander44 <sikander@wistech.biz>
2024-11-30 12:28:43 +0500

    fixes

--- resources/views/admin/Dashboard.blade.php
+++ resources/views/admin/Dashboard.blade.php
@@ -15,7 +15,16 @@
                                 <h5 class="ms-3 mt-3">Student</h5>
                                 <p class="ms-3 mt-2">Total Students</p>
                                 <a class="h5 text-white fw-bold stretched-link ms-3 mb-3 text-decoration-none"
-
+                                    href="{{ route('applications') }}">{{ $students }}</a>
+                            </div>
+                        </div>
+                        <div class="col-md-3">
+                            <div class="card text-white rounded-2 bg-gradient"
+                                style="border: none; background-color: #DC4BF1;">
+                                <h5 class="ms-3 mt-3">Users</h5>
+                                <p class="ms-3 mt-2">Total Users</p>
+                                <a class="h5 text-white fw-bold stretched-link ms-3 mb-3 text-decoration-none"
+                                    href="{{ route('users') }}">{{ $users_count }}</a>
                             </div>
                         </div>
                         <div class="col-md-3">
@@ -24,7 +33,7 @@
                                 <h5 class="ms-3 mt-3">Teachers</h5>
                                 <p class="ms-3 mt-2">Total Teachers</p>
                                 <a class="h5 text-white fw-bold stretched-link ms-3 mb-3 text-decoration-none"
-
+                                    href="#">0</a>
                             </div>
                         </div>
                         <div class="col-md-3">
@@ -32,7 +41,7 @@
                                 <h5 class="ms-3 mt-3">Parents</h5>
                                 <p class="ms-3 mt-2">Total Parents</p>
                                 <a class="h5 text-white fw-bold stretched-link ms-3 mb-3 text-decoration-none"
-
+                                    href="#">0</a>
                             </div>
                         </div>
 
@@ -41,6 +50,8 @@
             </div>
         </div>
     </div>
+
+
 
     <!-- <div class="row">
         <div class="col-xl-6">
