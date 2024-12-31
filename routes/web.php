7b38e5632e8b9469c87229bf87bd340afb593746
RanaSikander44 <sikander@wistech.biz>
2024-12-31 17:52:54 +0500

    push

--- routes/web.php
+++ routes/web.php
@@ -163,10 +163,15 @@ Route::middleware(['auth'])->group(function () {
     Route::post('chid/activities/filter', [ChildActiviesController::class, 'filter'])->name('filter.activity');
 
 
-
     // Dormitory
     Route::get('room-list', [DormitoryController::class, 'index'])->name('room-list');
-    Route::get('add-room', [DormitoryController::class, 'store'])->name('add-room');
+    Route::get('add/room', [DormitoryController::class, 'add'])->name('add-room');
+    Route::post('add/room/store', [DormitoryController::class, 'store'])->name('room-store');
+    Route::get('add/room/edit/{id}', [DormitoryController::class, 'edit'])->name('room.edit');
+    Route::post('add/room/update/{id}', [DormitoryController::class, 'update'])->name('room.update');
+    Route::get('add/room/delete/{id}', [DormitoryController::class, 'delete'])->name('room.delete');
+
+
 
 });
 
